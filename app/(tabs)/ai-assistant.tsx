04a1d35db15d02b5b6706b370922b9f9f05bb956
Marcelo <rodrigomarcelo643@gmail.com>
2025-09-23 21:40:53 +0800

    update: changed image on ai

--- app/(tabs)/ai-assistant.tsx
+++ app/(tabs)/ai-assistant.tsx
@@ -476,7 +476,7 @@ export default function AIAssistant() {
         <View className="flex-row items-center">
           <View className="w-10 h-10 rounded-full items-center justify-center mr-3">
             <Image
-                source={require("../../assets/images/chatbot-app.png")}
+                source={require("../../assets/images/chatbot.png")}
                 className="w-10 h-10"
             />
           </View>
