012dbe64b337ee27c524fb8f2f692fe6b04b9e68
rodrigomarcelo643 <159506698+rodrigomarcelo643@users.noreply.github.com>
2026-02-20 15:29:39 +0800

    docs: add ML Image Classifier API BASE URL to configuration

--- constants/Config.ts
+++ constants/Config.ts
@@ -3,3 +3,8 @@
  * @Author: Marcelo Rodrigo
  */
 export const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL!;
+
+/**
+ * ML Image Classifier API BASE URL (Render)
+ */
+export const ML_API_BASE_URL = process.env.EXPO_PUBLIC_ML_API_BASE_URL!;
