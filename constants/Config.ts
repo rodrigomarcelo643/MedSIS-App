159a54728f1e4d91231e7244fb1d1cf1a1237e06
Marcelo <rodrigomarcelo643@gmail.com>
2026-04-12 13:01:24 +0800

    chore: add trim for base url incaase future network error occurs

--- constants/Config.ts
+++ constants/Config.ts
@@ -2,7 +2,7 @@
  * Backend API BASE URL
  * @Author: Marcelo Rodrigo
  */
-export const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL!;
+export const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL?.trim()!;
 
 /**
  * ML Image Classifier API BASE URL (Render)
