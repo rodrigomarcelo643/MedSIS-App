3df362549a86ef08974774053829994310e6d770
rodrigomarcelo643 <159506698+rodrigomarcelo643@users.noreply.github.com>
2026-07-06 13:14:26 +0800

    refactor(home): apply code formatting to WelcomeHeader component

--- components/home/WelcomeHeader.tsx
+++ components/home/WelcomeHeader.tsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { useThemeColor } from "@/hooks/useThemeColor";
-import { WelcomeHeaderProps } from '@/@types/tabs';
+import { WelcomeHeaderProps } from "@/@types/tabs";
+import React from "react";
+import { Text, View } from "react-native";
 
-export const WelcomeHeader: React.FC<WelcomeHeaderProps & { mutedColor: string }> = ({
-  user,
-  mutedColor
-}) => {
+export const WelcomeHeader: React.FC<
+  WelcomeHeaderProps & { mutedColor: string }
+> = ({ user, mutedColor }) => {
   return (
-    <View className="flex-row justify-between p-2 items-center mb-2">
+    <View className="flex-row justify-between p-2 items-center ">
       <View className="flex-1">
         <Text style={{ fontSize: 16, color: mutedColor, marginBottom: 4 }}>
           Welcome back,
         </Text>
-        <Text style={{ fontSize: 30, fontWeight: 'bold', color: '#dc2626' }}>
+        <Text style={{ fontSize: 30, fontWeight: "bold", color: "#dc2626" }}>
           {user?.first_name} {user?.last_name}
         </Text>
       </View>
